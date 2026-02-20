2ed96bb9a3240cde6c88197e7a3490cc6209e53f
OpenSourceWarrior <foundr4u@gmail.com>
2026-02-20 21:29:07 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/').pop();
+
 export default defineConfig({
+  base: repoName ? `/${repoName}/` : '/',
   plugins: [react()],
   resolve: {
     alias: {
